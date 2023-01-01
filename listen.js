a4faf99edd7984485788f2fdd71f873d3bdee58f
tate <tate.sharp@gmail.com>
2023-01-01 17:17:11 -0400

    Implement close/open position logging, tg wip

--- listen.js
+++ listen.js
@@ -1,20 +1,31 @@
 import { listen } from "binance-leaderboard-listener";
 // Exactly the same as scan.js
 // Reminder: Uninstall in order to not show weakness
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const users = {
   TreeOfAlpha1: {
+    name: "TreeOfAlpha",
     encryptedUid: "FB23E1A8B7E2944FAAEC6219BBDF8243",
     positions: [],
     initialized: false,
   },
   TreeOfAlpha2: {
+    name: "TreeOfAlpha",
     encryptedUid: "DF74DFB6CB244F8033F1D66D5AA0B171",
     positions: [],
     initialized: false,
   },
   blankbrain: {
+    name: "BlankBrain",
     encryptedUid: "10CD59AFCF8A261D738E3803586FE3FF",
+    positions: [],
+    initialized: false,
+  },
+  hyuk: {
+    name: "Hyuk",
+    encryptedUid: "0F773DC158FDF10D9B0FDDC860C5FD2C",
     positions: [],
     initialized: false,
   },
@@ -23,9 +34,28 @@ const users = {
 
 const updatedPositions = (prev, curr) => {
   let temp = [];
-  if (prev.length != curr.length) {
-    // Look for Open / Close events ici
-    return curr;
+  if (prev.length < curr.length) {
+    // Opened a position
+    const dir = curr[0].amount > 0 ? "LONG" : "SHORT";
+    const value = curr[0].amount * curr[0].entryPrice;
+    const margin = value / curr[0].leverage;
+
+    console.log(
+      `${users.blankbrain.name} opened \$${margin} ${dir} on ${curr[0].symbol} @ \$${curr[0].entryPrice} (${value} w/ ${curr[0].leverage}x lev)`,
+    );
+    //} else if (true) {
+  } else if (prev.length > curr.length) {
+    // Closed a position
+    const closed_position = Object.values(prev).filter((val) => {
+      return !JSON.stringify(curr).includes(JSON.stringify(val));
+    });
+    const dir = closed_position[0].amount > 0 ? "LONG" : "SHORT";
+    const value = curr[0].amount * curr[0].entryPrice;
+    const margin = value / curr[0].leverage;
+
+    console.log(
+      `${users.blankbrain.name} closed \$${margin} ${dir} on ${curr[0].symbol} @ \$${curr[0].entryPrice} (${value} w/ ${curr[0].leverage}x lev)`,
+    );
   } else {
     // console.log(Object.entries(curr));
     temp = Object.entries(curr).forEach(([key, val]) => {
@@ -41,8 +71,9 @@ const updatedPositions = (prev, curr) => {
         /*
          * TESTING
          */
+        //console.log(val);
         console.log(
-          `[updated PnL for ${val.amount} ${val.symbol}]: \$${val.pnl}    -    prev: ${prev[key].pnl}, curr: ${val.pnl}`,
+          `[updated PnL for 'user' ${val.amount} ${val.symbol}]: \$${val.pnl}    -    prev: ${prev[key].pnl}, curr: ${val.pnl}    %    opened @ ${val.updateTime}`,
         );
         prev[key] = val;
         return 1;
@@ -51,19 +82,8 @@ const updatedPositions = (prev, curr) => {
   }
 };
 
-// const deepEqual = (a, b) => {
-//   if (a === b) return true;
-//   if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime();
-//   if (!a || !b || (typeof a !== "object" && typeof b !== "object")) return a === b;
-//   if (a === null || a === undefined || b === null || b === undefined) return false;
-//   if (a.prototype !== b.prototype) return false;
-//   // let keys = Object.keys(a);
-//   let keys = ['symbol', 'entryPrice', 'updateTime']
-//   // keys.filter(([key]) => key.includes(""))
-//   if (keys.length !== Object.keys(b).length) return false;
-//   return keys.every((k) => isEqual(a[k], b[k]));
-// };
-
+const tg = `https://api.telegram.org/bot${process.env.TG_API}/getMe`;
+console.log(tg);
 const listener = listen({
   encryptedUid: "4DEEE53F639E5E825DC84E6B7AF3D1E8",
   delay: 2000,
@@ -82,9 +102,8 @@ listener.on("update", (data) => {
   } else {
     // const eq = users.test.positions.isEqual(data);
     // const eq = comparePositions(users.test.positions, data);
-    const eq = JSON.stringify(users.test.positions) === JSON.stringify(data);
+    //const eq = JSON.stringify(users.test.positions) === JSON.stringify(data);
 
-    // console.log(Object.values(data));
     console.log(updatedPositions(users.test.positions, data));
     // if (updatedPositions(users.test.positions, data)) {
     //   console.log(comp);
